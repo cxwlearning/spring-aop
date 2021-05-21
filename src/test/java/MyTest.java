0a06c674ed400e7938d438b21a7d62592fd6e57d
caoxiwen <425631891@163.com>
2021-05-21 16:27:16 +0800

    'test2'

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -7,7 +7,7 @@ public class MyTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        //动态代理代理的是接口
+        //动态代理代理的是接口.
         UserService userService = (UserService) context.getBean("userService");
         userService.select();
     }
